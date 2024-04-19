32618d435200bc6e34a539fe469669fe80623c10
Centralice <108938330+Centralice@users.noreply.github.com>
2024-04-20 00:19:31 +0200

    task-4 in progress

--- js/task-2.js
+++ js/task-2.js
@@ -12,67 +12,61 @@
 // Метод filter() можна використовувати для створення нового масиву з елементами, які задовольняють певну умову.
 // Використовуй метод includes() для перевірки, чи масив friends містить friendName.
 
-function formatMessage(message, maxLength) {
-  if (message.length <= maxLength) {
-return message;
-  } else {
-return `${message.slice(0, maxLength)}...`;
-  }  
-}
+const getUsersWithFriend = (users, friendName) => users.filter((user) => user.friends.includes(friendName));
 
-// const allUsers = [
-//   {
-//     name: "Moore Hensley",
-//     friends: ["Sharron Pace"]
-//   },
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    friends: ["Sharron Pace"]
+  },
+  {
+    name: "Sharlene Bush",
+    friends: ["Briana Decker", "Sharron Pace"]
+  },
+  {
+    name: "Ross Vazquez",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+  },
+  {
+    name: "Elma Head",
+    friends: ["Goldie Gentry", "Aisha Tran"]
+  },
+  {
+    name: "Carey Barr",
+    friends: ["Jordan Sampson", "Eddie Strong"]
+  },
+  {
+    name: "Blackburn Dotson",
+    friends: ["Jacklyn Lucas", "Linda Chapman"]
+  },
+  {
+    name: "Sheree Anthony",
+    friends: ["Goldie Gentry", "Briana Decker"]
+  }
+];
+
+console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+// [
 //   {
 //     name: "Sharlene Bush",
 //     friends: ["Briana Decker", "Sharron Pace"]
 //   },
 //   {
-//     name: "Ross Vazquez",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
-//   },
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
 //   {
 //     name: "Elma Head",
 //     friends: ["Goldie Gentry", "Aisha Tran"]
 //   },
 //   {
-//     name: "Carey Barr",
-//     friends: ["Jordan Sampson", "Eddie Strong"]
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"]
-//   },
-//   {
 //     name: "Sheree Anthony",
 //     friends: ["Goldie Gentry", "Briana Decker"]
 //   }
-// ];
+// ]
 
-// console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
-// // [
-// //   {
-// //     name: "Sharlene Bush",
-// //     friends: ["Briana Decker", "Sharron Pace"]
-// //   },
-// //   {
-// //     name: "Sheree Anthony",
-// //     friends: ["Goldie Gentry", "Briana Decker"]
-// //   }
-// // ]
-
-// console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-// // [
-// //   {
-// //     name: "Elma Head",
-// //     friends: ["Goldie Gentry", "Aisha Tran"]
-// //   },
-// //   {
-// //     name: "Sheree Anthony",
-// //     friends: ["Goldie Gentry", "Briana Decker"]
-// //   }
-// // ]
-
-// console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
--- js/task-4.js
+++ js/task-4.js
@@ -7,66 +7,52 @@
 // та повертати загальний баланс користувачів (властивість balance),
 // стать яких (властивість gender) збігається зі значенням параметра gender.
 
-function getShippingCost(country) {
-  let price;
-  switch (country) {
-    case 'China':
-      price = 100;
-      break;
-    case 'Chile':
-      price = 250;
-      break;
-    case 'Australia':
-      price = 170;
-      break;
-    case 'Jamaica':
-      price = 120;
-      break;
-    default:
-      return 'Sorry, there is no delivery to your country';
+const getTotalBalanceByGender = (users, gender) => users.reduce((acc, user) => {
+  if(user.gender === gender) {
+   acc + user.balance
   }
-  return `Shipping to ${country} will cost ${price} credits`;
-}
+  return acc + user.balance;
+}, 0);
 
-// const clients = [
-// 	{
-//     name: "Moore Hensley",
-//     gender: "male",
-//     balance: 2811
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     gender: "female",
-//     balance: 3821
-//   },
-//   {
-//     name: "Ross Vazquez",
-//     gender: "male",
-//     balance: 3793
-//   },
-//   {
-//     name: "Elma Head",
-//     gender: "female",
-//     balance: 2278
-//   },
-//   {
-//     name: "Carey Barr",
-//     gender: "male",
-//     balance: 3951
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     gender: "male",
-//     balance: 1498
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     gender: "female",
-//     balance: 2764
-//   }
-// ];
+const clients = [
+	{
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
 
-// console.log(getTotalBalanceByGender(clients, "male")); // 12053
+console.log(getTotalBalanceByGender(clients, "male")); // 12053
 
-// console.log(getTotalBalanceByGender(clients, "female")); // 8863
+console.log(getTotalBalanceByGender(clients, "female")); // 8863
 
